a0039b36468e15101d418df02c27ce90b11dd698
bo1atito <shorungbebolatito@gmail.com>
2024-12-07 02:58:25 +0100

    correct errors

--- markdown2html.py
+++ markdown2html.py
@@ -3,13 +3,15 @@
 import sys
 import os
 
-if len(sys.argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
 
-    mark_down_file = sys.argv[1]
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
     html_file = sys.argv[2]
 
-    if not os.path.exists(mark_down_file):
-        print(f"Missing {mark_down_file}")
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
